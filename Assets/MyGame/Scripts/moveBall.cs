53ade2f58bce2a6fe862e055242fdb9d8d297e8b
mopvp <moritz.niedermaier01@htl-salzburg.ac.at>
2020-01-14 13:03:13 +0100

    add Script edit UI

---
+++ Assets/MyGame/Scripts/moveBall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class moveBall : MonoBehaviour
+{
+    public GameObject Ball;
+    [SerializeField] private GameObject[] BallPositions;
+
+
+    private int RandomNumber()
+    {
+        return Random.Range(0, 4);
+    }
+
+    public void secondNumber()
+    {
+        Ball.transform.position = BallPositions[RandomNumber()].transform.position;
+
+    }
+}
+
